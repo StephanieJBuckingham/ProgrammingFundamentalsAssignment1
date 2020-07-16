2072d5f101796f180177c65802418fcd7814ceb1
StephanieJBuckingham <68093533+StephanieJBuckingham@users.noreply.github.com>
2020-07-16 13:27:21 -0400

    Second Commit

--- Assignment1/src/Cipher.java
+++ Assignment1/src/Cipher.java
@@ -1,15 +1,25 @@
 import java.util.*;
+import java.awt.Font;
+import java.text.*;
 
-//Stephanie Buckingham
-//Programming Fundamentals
-//Summer Session II
-//Assignment 1 
+/* Stephanie Buckingham
+Programming Fundamentals
+Summer Session II
+Assignment 1  */ 
 
-public class Assignment1 {
+public class Cipher {
 
 	public static void main(String[] args) {
-		System.out.println("Enter 5 integers between 0 and 19.");
+		
+		//Welcome to Cipher Program
+		System.out.println("Welocome to the Cipher Program");
+		System.out.println();
+		
+		//Prompt for 5 numbers 
+		System.out.println("Enter 5 numbers between 0 and 19.");
 		Scanner scan = new Scanner(System.in);
+		
+		//Initialize numbers
 		int int1, int2, int3, int4, int5;
 		int1 = scan.nextInt();
 		int2 = scan.nextInt();
@@ -17,51 +27,33 @@ public class Assignment1 {
 		int4 = scan.nextInt();
 		int5 = scan.nextInt();
 
+		//Prompt if entered incorrect numbers
 		if (int1 > 19 || int2 > 19 || int3 > 19 || int4 > 19 || int5 > 19) {
 			System.out.println("Please read the directions and try again.");
 			System.exit(0);
 		}
-
 		scan.close();
 
-		int uncodednum, uncodenumtens, uncodenumones;
-		uncodednum = int1 + int2 + int3 + int4 + int5;
-		System.out.println(uncodednum);
+		//Initialize unencoded number, the tens digit and ones digit
+		int unencodednumber, tensplace, onesplace;
+		unencodednumber = int1 + int2 + int3 + int4 + int5;
+		tensplace = Math.round(unencodednumber / 10);
+		onesplace = Math.round(unencodednumber % 10);
 
-		uncodenumtens = Math.round(uncodednum / 10);
-		uncodenumones = Math.round(uncodednum % 10);
 
-		int int1tens;
-		int int1ones;
-		int int2tens;
-		int int2ones;
-		int int3tens;
-		int int3ones;
-		int int4tens;
-		int int4ones; 
-		int int5tens;
-		int int5ones;
-		int1tens = Math.round(int1 / 10);
-		int1ones = Math.round(int1 % 10);
-		int2tens = Math.round(int2 / 10);
-		int2ones = Math.round(int2 % 10);
-		int3tens = Math.round(int3 / 10);
-		int3ones = Math.round(int3 % 10);
-		int4tens = Math.round(int4 / 10);
-		int4ones = Math.round(int4 % 10);
-		int5tens = Math.round(int5 / 10);
-		int5ones = Math.round(int5 % 10);
-
+		//Generate key
 		Random gen = new Random();
 		int key;
 		key = gen.nextInt(9);
 		System.out.println(key);
 
+		//
 		int encodedtens, encodedones, codednum;
-		encodedtens = (uncodenumtens + key) % 10;
-		encodedones = (uncodenumones + key) % 10;
+		encodedtens = (tensplace + key) % 10;
+		encodedones = (onesplace + key) % 10;
 		codednum = (encodedtens * 10) + encodedones;
-		System.out.println(codednum);
+		System.out.println();
+		System.out.println("Your encoded number is " + codednum);
 
 	}
 }
