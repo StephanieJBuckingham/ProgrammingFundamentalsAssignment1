154da963022d3a59b88afa396a8d7fd8a58edc4f
StephanieJBuckingham <68093533+StephanieJBuckingham@users.noreply.github.com>
2020-07-17 06:55:53 -0400

    Final Commit

--- Assignment1/src/Cipher.java
+++ Assignment1/src/Cipher.java
@@ -17,7 +17,7 @@ public class Cipher {
 		System.out.println("Enter 5 numbers between 0 and 19.");
 		Scanner scan = new Scanner(System.in);
 
-		// Initialize numbers
+		// Initialize numbers 
 		int int1, int2, int3, int4, int5;
 		int1 = scan.nextInt();
 		int2 = scan.nextInt();
@@ -26,7 +26,7 @@ public class Cipher {
 		int5 = scan.nextInt();
 
 		// Prompt if entered incorrect numbers
-		if (int1 > 19 || int2 > 19 || int3 > 19 || int4 > 19 || int5 > 19) {
+		if ( int1 > 19 || int1 < 0 || int2 > 19 || int2 < 0 || int3 > 19 || int3 < 0|| int4 > 19 || int4 < 0 || int5 > 19 || int5 < 0) {
 			System.out.println("Please read the directions and try again.");
 			System.exit(0);
 		}
@@ -35,14 +35,15 @@ public class Cipher {
 		// Initialize unencoded number, the tens digit and ones digit
 		int unencodednumber, tensplace, onesplace;
 		unencodednumber = int1 + int2 + int3 + int4 + int5;
-		tensplace = Math.round(unencodednumber / 10);
+		tensplace = unencodednumber / 10;
 		onesplace = unencodednumber % 10;
 
-		// Generate key
+		// Generate key and print
 		Random gen = new Random();
 		int key;
 		key = gen.nextInt(9);
-		System.out.println(key);
+		System.out.println();
+		System.out.println("You encoding key is " + key);
 
 		// Generate encoded number and print
 		int encodedtens, encodedones, codednum;
